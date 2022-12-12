aa5ac34e439ba7579254780e0fe251f8c153e143
withpaul <jongjin4749@gmail.com>
2022-12-12 23:34:11 +0900

    getmapping 해봄

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,16 +1,46 @@
 package hello.hellospring.controller;
 
-import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
-    @Autowired
-    public String hello(MemberService memberService) {
-        return "hello";
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("age") int age, Model model)
+    {
+        model.addAttribute("age", age);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-mvc-required")
+    public String helloMvcRequired(@RequestParam(value = "age", required = false) Integer age, Model model)
+    {
+        model.addAttribute("age", age);
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-mvc-default")
+    public String helloMvcDefault(@RequestParam(value = "age", defaultValue = "5") Integer age, Model model)
+    {
+        model.addAttribute("age", age);
+        return "hello-mvc";
+    }
+
+
+    /**
+     * age는 필수 요소이다
+     * 이름은 필수는 아니고 디폴트는 김종진으로
+     */
+    @GetMapping("test")
+    public String test(
+        @RequestParam("age") Integer age,
+        @RequestParam(value = "name", required = false, defaultValue = "김종진") String name,
+        Model model
+    ) {
+        model.addAttribute("age", age);
+        model.addAttribute("name", name);
+        return "test";
     }
 }
