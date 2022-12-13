c259edcbe128289f7458d710c3ac889b432ed6a7
withpaul <jongjin4749@gmail.com>
2022-12-13 22:40:19 +0900

    json api 만들어보기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -42,5 +43,40 @@ public class HelloController {
         model.addAttribute("age", age);
         model.addAttribute("name", name);
         return "test";
+    }
+
+    @GetMapping("hello-integer-year")
+    @ResponseBody
+    public Integer returnIntegerYear(@RequestParam(name = "year", required = false, defaultValue = "2022") Integer year)
+    {
+        return year;
+    }
+
+    @GetMapping("hello-object-person")
+    @ResponseBody
+    public Person returnHelloObjectPerson(
+            @RequestParam(name="age", required = false, defaultValue = "32") Integer age,
+            @RequestParam("name") String name
+    )  {
+        // getter가 없으면 HttpMediaTypeNotAcceptableException 에러남
+        return new Person(age, name);
+    }
+
+    static class Person {
+        private Integer age;
+        private String name;
+
+        public Person(Integer age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
